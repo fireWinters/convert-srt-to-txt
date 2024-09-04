28cc5d8f07eac4b9cf046360bbbb6834e90811d7
Diana Tang <1987567948@qq.com>
2024-09-04 20:02:29 +0800

    feat: I want to test

---
+++ sotired.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: Diana Tang
+ * @Date: 2024-09-04 19:53:23
+ * @LastEditors: Diana Tang
+ * @Description: some description
+ * @FilePath: /convert-srt-to-txt/sotired.js
+ */
+这都什么破烂，练不动
